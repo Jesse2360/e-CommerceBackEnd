59b723f57dcff9e504e8486e5440dcd9725b554f
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-06-05 01:13:55 -0500

    updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,6 +15,8 @@ router.get('/', async (req, res) => {
         },
       ],
     });
+
+    res.status(200).json(categoryData)
    } catch (err) {
      console.log(err);
      res.status(500).json(err);
